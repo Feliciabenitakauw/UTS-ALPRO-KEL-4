f30b25d67017c8662e54525b0ec6fb9a712ba930
322510014@student.machung.ac.id <322510014@student.machung.ac.id>
2025-10-30 10:26:18 +0700

    update

---
+++ Bilangan_ganjilgenap.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout <<"Masukkan sebuah bilangan: ";
+    cin >> n;
+
+    if (n % 2 == 0) {
+        cout << n << "Bilangan genap." << endl;
+    } else {
+        cout << n << "Bilangan Ganjil." << endl;
+        
+    }
+    return 0;
+        
+    }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
